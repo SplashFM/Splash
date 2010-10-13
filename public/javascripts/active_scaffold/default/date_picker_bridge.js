630f93a45a942812ac6af776a3ef36057caa11ae
Matthew Forsyth <matt@mojotech.com>
2010-10-13 16:33:30 -0400

    Included additional javascript produced by active_scaffold.

---
+++ public/javascripts/active_scaffold/default/date_picker_bridge.js
@@ -0,0 +1,32 @@
+jQuery(function($){
+  if (typeof($.datepicker) === 'object') {
+    $.datepicker.regional['en'] = {"weekHeader":"Wk","changeMonth":true,"monthNamesShort":["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],"firstDay":0,"closeText":"Close","dayNames":["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],"isRTL":false,"prevText":"Previous","dayNamesShort":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"dateFormat":"yy-mm-dd","showMonthAfterYear":false,"nextText":"Next","dayNamesMin":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"currentText":"Today","changeYear":true,"monthNames":["January","February","March","April","May","June","July","August","September","October","November","December"]};
+    $.datepicker.setDefaults($.datepicker.regional['en']);
+  }
+  if (typeof($.timepicker) === 'object') {
+    $.timepicker.regional['en'] = {"timeFormat":"hh:mm:ss","hourText":"Hour","minuteText":"Minute","secondText":"Seconds","dateFormat":"D, dd M yy ","ampm":false};
+    $.timepicker.setDefaults($.timepicker.regional['en']);
+  }
+});
+$(document).ready(function() {
+  $('input.date_picker').live('focus', function(event) {
+    var date_picker = $(this);
+    if (typeof(date_picker.datepicker) == 'function') {
+      if (!date_picker.hasClass('hasDatepicker')) {
+        date_picker.datepicker();
+        date_picker.trigger('focus');
+      }
+    }
+    return true;
+  });
+  $('input.datetime_picker').live('focus', function(event) {
+    var date_picker = $(this);
+    if (typeof(date_picker.datetimepicker) == 'function') {
+      if (!date_picker.hasClass('hasDatepicker')) {
+        date_picker.datetimepicker();
+        date_picker.trigger('focus');
+      }
+    }
+    return true;
+  });
+});
