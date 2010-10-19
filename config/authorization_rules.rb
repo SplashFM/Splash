ff78b4af5d01d5d2d555afc91fbed085154144e7
Chris Shoemaker <chris@mojotech.com>
2010-10-18 23:42:14 -0400

    Expand the scope of "active_scaffold" privileges to include editing associations.

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -17,5 +17,5 @@ privileges do
   privilege :create, :includes => :new
   privilege :update, :includes => :edit
   privilege :delete, :includes => :destroy
-  privilege :active_scaffold, :includes => [:manage, :impersonate, :deimpersonate, :update_column, :row, :show_search, :nested, :table]
+  privilege :active_scaffold, :includes => [:manage, :impersonate, :deimpersonate, :update_column, :row, :show_search, :nested, :table, :edit_associated]
 end
