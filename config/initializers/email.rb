08ad87998b6962a1477c601d1529c1e159216ace
Matthew Forsyth <matt@mojotech.com>
2010-10-21 16:59:58 -0400

    Tell apps to use Sendgrid for sending email.
    Expect sendgrid login to be in config.yml.

---
+++ config/initializers/email.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.default_charset = "utf-8"
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :domain => "mojotech.com",
+  :authentication => :login,
+  :user_name => AppConfig.sendgrid['username'],
+  :password => AppConfig.sendgrid['password'],
+  :dev_mailto => "dev@mojotech.com"
+}
