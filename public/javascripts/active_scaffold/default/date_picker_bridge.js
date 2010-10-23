9a63ff850c4629823e54d916452bbfe1dced5049
Matthew Forsyth <matt@mojotech.com>
2010-10-23 16:26:55 -0400

    Remove javascript that activescaffold seems to auto-delete.

--- public/javascripts/active_scaffold/default/date_picker_bridge.js
+++ public/javascripts/active_scaffold/default/date_picker_bridge.js
@@ -1,6 +1,6 @@
 jQuery(function($){
   if (typeof($.datepicker) === 'object') {
-    $.datepicker.regional['en'] = {"dayNamesShort":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"dateFormat":"yy-mm-dd","showMonthAfterYear":false,"nextText":"Next","dayNamesMin":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"currentText":"Today","changeYear":true,"monthNames":["January","February","March","April","May","June","July","August","September","October","November","December"],"weekHeader":"Wk","changeMonth":true,"monthNamesShort":["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],"firstDay":0,"closeText":"Close","dayNames":["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],"isRTL":false,"prevText":"Previous"};
+    $.datepicker.regional['en'] = {"dayNamesShort":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"dateFormat":"yy-mm-dd","nextText":"Next","dayNamesMin":["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],"weekHeader":"Wk","currentText":"Today","changeYear":true,"firstDay":0,"monthNames":["January","February","March","April","May","June","July","August","September","October","November","December"],"changeMonth":true,"isRTL":false,"monthNamesShort":["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],"closeText":"Close","showMonthAfterYear":false,"dayNames":["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],"prevText":"Previous"};
     $.datepicker.setDefaults($.datepicker.regional['en']);
   }
   if (typeof($.timepicker) === 'object') {
