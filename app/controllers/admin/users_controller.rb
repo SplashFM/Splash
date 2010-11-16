830d0e10c27acf640adf21c589940844db2f3035
Chris Shoemaker <chris@mojotech.com>
2010-11-15 20:56:02 -0500

    Add a sample usage of active_scaffold_export for exporting Users.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,6 +13,13 @@ class Admin::UsersController < Admin::AdminController
 
     config.action_links.add 'impersonate', :label => 'Impersonate',
       :page => true, :type => :member, :method => :post
+
+    # active_scaffold_export
+    actions.add :export
+    config.export.columns = [ :id ] + base_cols
+    config.export.default_deselected_columns = [ :avatar ]
+    config.export.default_delimiter = ','
+    config.export.force_quotes = true
   end
 
   def impersonate
