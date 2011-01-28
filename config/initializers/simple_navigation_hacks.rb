19c12607d8ca2ef1e501fd6cc3b8df75b1377f0e
Chris Shoemaker <chris@mojotech.com>
2011-01-28 17:50:45 -0500

    Indentation fix.

--- config/initializers/simple_navigation_hacks.rb
+++ config/initializers/simple_navigation_hacks.rb
@@ -2,7 +2,7 @@ module SimpleNavigation
   module Renderer
     class List < SimpleNavigation::Renderer::Base
       def render( item_container )
-          list_content = item_container.items.inject([]) do |list, item|
+        list_content = item_container.items.inject([]) do |list, item|
           html_options = item.html_options
           li_content = link_to(content_tag(:span, item.name), item.url, :class => item.selected_class, :method => item.method)
           if include_sub_navigation?(item)
