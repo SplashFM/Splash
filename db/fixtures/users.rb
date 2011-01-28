2dfaea30fb63298df1a4587eeb2b03ffbc593795
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2011-01-28 11:57:23 -0500

    update user seeds fixtures

--- db/fixtures/users.rb
+++ db/fixtures/users.rb
@@ -1,7 +1,24 @@
+# superuser account
 User.seed(:email) do |user|
   user.email = 'dev@mojotech.com'
-  user.encrypted_password ="$2a$10$k0OiI63t7F3haOiMNC61keVt.g1QgAOElFCw8wcYQePdbwOdREFBu"
-  user.password_salt = "$2a$10$k0OiI63t7F3haOiMNC61ke"
+  if Rails.env.production?
+    user.encrypted_password ="$2a$10$k0OiI63t7F3haOiMNC61keVt.g1QgAOElFCw8wcYQePdbwOdREFBu"
+    user.password_salt = "$2a$10$k0OiI63t7F3haOiMNC61ke"
+  else
+    user.encrypted_password = '$2a$10$HgMJqHP9ddNv.BEbWntYleKdVzJijjmnlyUBTkmUIYdj4AzwM9Iha' # password
+    user.password_salt = '$2a$10$HgMJqHP9ddNv.BEbWntYle'
+  end
   user.superuser = true
   user.confirmed_at = Time.now
+end
+
+unless Rails.env.production?
+  # generic user account
+  User.seed(:email) do |user|
+    user.email = 'user@mojotech.com'
+    user.encrypted_password = '$2a$10$HgMJqHP9ddNv.BEbWntYleKdVzJijjmnlyUBTkmUIYdj4AzwM9Iha' # password
+    user.password_salt = '$2a$10$HgMJqHP9ddNv.BEbWntYle'
+    user.superuser = false
+    user.confirmed_at = Time.now
+  end
 end
