625340c24205cab6b50aeffc7d72604630664e25
Chris Shoemaker <chris@mojotech.com>
2011-02-03 20:57:49 -0500

    Update navigation definition.
    simple-navigation expects this argument to be a string.

--- config/navigation.rb
+++ config/navigation.rb
@@ -6,7 +6,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
     primary.item :home, 'Home', home_url
 
-    primary.item :users_admin, 'Users', {:controller => 'admin/users'},
+    primary.item :users_admin, 'Users', admin_users_url,
       :class => 'special',
       :if => Proc.new{ signed_in_as_superuser? }
 
