2a680c2a7f9ff468c1b06a3e3f7b60164f4ea8ec
Chris Shoemaker <chris@mojotech.com>
2011-02-11 00:42:37 -0500

    Updates to rails base files from "rake rails:update"
    This includes an important update for AJAX CSRF protection.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@ Rails.application.config.session_store :cookie_store,
   :key => "_#{Rails.application.name.downcase}_session"
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
+# (create the session table with "rails generate session_migration")
 # Rails.application.config.session_store :active_record_store
