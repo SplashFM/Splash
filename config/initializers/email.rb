70c8baa70b3d5d4171f98e2534ecab318e25c0be
Chris Shoemaker <chris@mojotech.com>
2011-03-28 12:51:56 -0400

    Update ActionMailer settings for test environment.

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,10 +1,13 @@
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.default :charset => "UTF-8"
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.sendgrid.net",
-  :domain => "mojotech.com",
-  :authentication => :login,
-  :user_name => AppConfig.sendgrid['username'],
-  :password => AppConfig.sendgrid['password'],
-  :dev_mailto => "dev@mojotech.com"
-}
+
+unless Rails.env.test?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :domain => "mojotech.com",
+    :authentication => :login,
+    :user_name => AppConfig.sendgrid['username'],
+    :password => AppConfig.sendgrid['password'],
+    :dev_mailto => "dev@mojotech.com"
+  }
+end
