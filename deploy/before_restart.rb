2c82e5e48a88c059b6bfcf462239f64aea5e2aba
David <david@mojotech.com>
2011-08-08 10:55:49 +0100

    Add sass and jammit hooks to EY deploy.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -6,3 +6,6 @@ if ['solo', 'util'].include?(node[:instance_role])
     run "cd #{release_path} && #{whenever_command} --update-crontab '#{app}' --set environment=#{@rails_env}"
   end
 end
+
+run "cd #{release_path} && bundle exec rake RAILS_ENV=#{environment} sass:compile"
+run "cd #{release_path} && RAILS_ENV=#{environment} bundle exec jammit  --base-url http://ec2-107-20-221-189.compute-1.amazonaws.com"
