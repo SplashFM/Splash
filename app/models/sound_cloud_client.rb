12005d28f0d73ea72cc71d1ef0b7c67512c202d6
David <david@mojotech.com>
2011-08-11 17:03:06 +0100

    Tweak formatting in SoundCloudClient#search.

--- app/models/sound_cloud_client.rb
+++ app/models/sound_cloud_client.rb
@@ -1,16 +1,17 @@
 class SoundCloudClient
   def search(query, opts)
     @sc.get('/tracks', :q => query, :order => 'hotness').
-      select { |e| query.split(/(\s+|,)/).all? { |w| e.title.downcase =~ /#{w}/ } }.
-      sort { |a, b|
-        a.favoritings_count.to_i <=> b.favoritings_count.to_i
-    }.reverse.first(opts[:limit]).map { |e|
-      Track.new(:favorites  => e.favoritings_count,
-                :plays      => e.playback_count,
-                :source     => 'soundcloud',
-                :title      => e.title,
-                :uri        => e.uri)
-    }
+      select { |e|
+        query.split(/(\s+|,)/).all? { |w| e.title.downcase =~ /#{w}/ } }.
+      sort { |a, b| a.favoritings_count.to_i <=> b.favoritings_count.to_i }.
+      reverse.
+      first(opts[:limit]).
+      map { |e|
+        Track.new(:favorites  => e.favoritings_count,
+                  :plays      => e.playback_count,
+                  :source     => 'soundcloud',
+                  :title      => e.title,
+                  :uri        => e.uri) }
   end
 
   private
