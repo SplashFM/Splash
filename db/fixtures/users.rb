d1fc7f108372c289e7072dead114586ea3f6c860
David <david@mojotech.com>
2011-09-01 21:25:26 +0100

    Stop using password salt for users.

--- db/fixtures/users.rb
+++ db/fixtures/users.rb
@@ -3,10 +3,8 @@ User.seed(:email) do |user|
   user.email = 'dev@mojotech.com'
   if Rails.env.production?
     user.encrypted_password ="$2a$10$k0OiI63t7F3haOiMNC61keVt.g1QgAOElFCw8wcYQePdbwOdREFBu"
-    user.password_salt = "$2a$10$k0OiI63t7F3haOiMNC61ke"
   else
     user.encrypted_password = '$2a$10$HgMJqHP9ddNv.BEbWntYleKdVzJijjmnlyUBTkmUIYdj4AzwM9Iha' # password
-    user.password_salt = '$2a$10$HgMJqHP9ddNv.BEbWntYle'
   end
   user.superuser = true
   user.confirmed_at = Time.now
@@ -17,7 +15,6 @@ unless Rails.env.production?
   User.seed(:email) do |user|
     user.email = 'user@mojotech.com'
     user.encrypted_password = '$2a$10$HgMJqHP9ddNv.BEbWntYleKdVzJijjmnlyUBTkmUIYdj4AzwM9Iha' # password
-    user.password_salt = '$2a$10$HgMJqHP9ddNv.BEbWntYle'
     user.superuser = false
     user.confirmed_at = Time.now
   end
