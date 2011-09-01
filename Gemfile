ba387cbb56946b66548c7233b482c78606c2c929
David <david@mojotech.com>
2011-09-01 21:42:52 +0100

    Disable braintree.

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'capistrano'
 # To minimize merge conflicts, keep this list sorted alphabetically
 gem "active_scaffold"
 gem "active_scaffold_export", ">= 3.0.6"
-gem "braintree"
+# gem "braintree"
 gem "daemons", "1.0.10"
 gem "declarative_authorization", ">= 0.5.1"
 gem "delayed_job"
--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,8 +1,9 @@
-unless AppConfig.braintree
-  raise 'You need a "braintree" stanza in your config.yml!  See the sample.'
+if defined?(Braintree)
+  unless AppConfig.braintree
+    raise 'You need a "braintree" stanza in your config.yml!  See the sample.'
+  end
+  Braintree::Configuration.environment = AppConfig.braintree['environment'].to_sym
+  Braintree::Configuration.merchant_id = AppConfig.braintree['merchant_id']
+  Braintree::Configuration.public_key = AppConfig.braintree['public_key']
+  Braintree::Configuration.private_key = AppConfig.braintree['private_key']
 end
-Braintree::Configuration.environment = AppConfig.braintree['environment'].to_sym
-Braintree::Configuration.merchant_id = AppConfig.braintree['merchant_id']
-Braintree::Configuration.public_key = AppConfig.braintree['public_key']
-Braintree::Configuration.private_key = AppConfig.braintree['private_key']
-
