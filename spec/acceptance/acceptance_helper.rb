655f13f797e8a19ccd6bf0631ecf456607c8bc27
David <david@mojotech.com>
2011-09-02 16:11:18 +0100

    Set default capybara javascript driver to webkit.

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -4,6 +4,8 @@ Spork.each_run do
   Dir[Rails.root.join("spec/acceptance/support/**/*.rb")].each { |f| require f }
 
   RSpec.configure do |config|
+    Capybara.javascript_driver = :webkit
+
     config.include Capybara::DSL
   end
 end
