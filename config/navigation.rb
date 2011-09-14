b0aa405b9279bf6a3d11ab1ad074f9415947d423
David Leal <david@mojotech.com>
2011-09-14 17:41:41 +0100

    Remove Tracks tab.

--- config/navigation.rb
+++ config/navigation.rb
@@ -5,8 +5,6 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
 
     primary.item :home, 'Home', home_url
-    primary.item :tracks, 'Tracks', tracks_url
-
   end
 
 end
