941c44d8bd83b26f85372e83406f9cfbf9c10376
Franco Brusatti <fbrusatti@gmail.com>
2011-09-19 20:12:30 -0300

    Add provider info to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110908173950) do
+ActiveRecord::Schema.define(:version => 20110919200248) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -35,11 +35,15 @@ ActiveRecord::Schema.define(:version => 20110908173950) do
   end
 
   create_table "tracks", :force => true do |t|
-    t.string   "title",      :null => false
+    t.string   "title",             :null => false
     t.string   "album"
-    t.string   "artist",     :null => false
+    t.string   "artist",            :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "data_file_name"
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.datetime "data_updated_at"
   end
 
   add_index "tracks", ["title", "artist"], :name => "index_tracks_on_title_and_artist", :unique => true
@@ -67,6 +71,8 @@ ActiveRecord::Schema.define(:version => 20110908173950) do
     t.datetime "avatar_updated_at"
     t.string   "type",                                :default => "User", :null => false
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
