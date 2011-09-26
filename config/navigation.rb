369d545baab6098b6a1a87d6b56a477b8935803c
Franco Brusatti <fbrusatti@gmail.com>
2011-09-26 10:28:09 -0300

    i18n navigation bar

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 # Configures your navigation
 SimpleNavigation::Configuration.run do |navigation|
-
   navigation.items do |primary|
-
-    primary.item :home, 'Home', home_url
+    primary.item :home, t('simple_navigation.menus.home'), home_url
   end
-
 end
-
