b3b8671e2e9d711075d62a775c19c19d6584266e
David Leal <david@mojotech.com>
2011-09-28 12:12:46 +0100

    [specs] Allow creating users in sequence.

--- spec/support/builders.rb
+++ spec/support/builders.rb
@@ -1,3 +1,5 @@
+user_seq = 0
+
 Bricks do
   builder Track do
     title  'Turn of the century'
@@ -6,17 +8,21 @@ Bricks do
   end
 
   builder User do
-    email        'user@mojotech.com'
+    email        { "user#{user_seq}@mojotech.com" }
     password     'testing'
     confirmed_at 1.day.ago
 
     trait :with_required_info do
-      name 'Mojo User'
+      name "Mojo User #{user_seq}"
     end
 
     trait :with_name do |n|
       email n.strip.downcase.gsub(/\s+/, '.') + "@userfactories.com"
       name  n
+    end
+
+    after :clone do
+      user_seq += 1
     end
   end
 end
