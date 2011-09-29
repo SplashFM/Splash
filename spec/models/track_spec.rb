87155a5c1281fc8bd4c028f8d16bd67f9323b6ed
David Leal <david@mojotech.com>
2011-09-29 11:33:15 +0100

    [specs] Enclose postrgres-specific Track specs in a describe block.

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,30 +1,32 @@
 require 'spec_helper'
 
 describe Track, :adapter => :postgresql do
-  it "is found by title" do
-    create(Track).title!('Close to the edge')
+  describe "searching", :adapter => :postgresql do
+    it "is found by title" do
+      create(Track).title!('Close to the edge')
 
-    Track.filtered('Close to the edge').should have(1).result
-  end
+      Track.filtered('Close to the edge').should have(1).result
+    end
 
-  it "is found by artist" do
-    create(Track).artist!('Yes')
+    it "is found by artist" do
+      create(Track).artist!('Yes')
 
-    Track.filtered('Yes').should have(1).result
-  end
+      Track.filtered('Yes').should have(1).result
+    end
 
-  it "is found by album" do
-    create(Track).album!('Relayer')
+    it "is found by album" do
+      create(Track).album!('Relayer')
 
-    Track.filtered('Relayer').should have(1).result
-  end
+      Track.filtered('Relayer').should have(1).result
+    end
 
-  it "may not be found" do
-    create(Track).
-      title('And you and I').
-      album('Close to the edge').
-      artist!('Yes')
+    it "may not be found" do
+      create(Track).
+        title('And you and I').
+        album('Close to the edge').
+        artist!('Yes')
 
-    Track.filtered('Fragile').should be_empty
+      Track.filtered('Fragile').should be_empty
+    end
   end
 end
