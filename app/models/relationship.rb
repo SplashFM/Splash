92c9da9b9ca9e9b737b38db5a6f8abfb42c9ddf1
Franco Brusatti <fbrusatti@gmail.com>
2011-09-30 15:17:48 -0300

    Notify user by email about a following action

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,4 +5,9 @@ class Relationship < ActiveRecord::Base
   validates :followed, :presence => true
   validates :follower, :presence => true
   validates_uniqueness_of :follower_id, :scope => [:followed_id]
+
+  after_create :send_following_notication
+  def send_following_notication
+    UserMailer.following(follower, followed).deliver
+  end
 end
