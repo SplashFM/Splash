09c23464cb3fc1bb845986a8d866e146f4215d06
David Leal <david@mojotech.com>
2011-09-30 10:44:05 +0100

    Decouple search result presentation from track.

---
+++ app/helpers/searches_helper.rb
@@ -0,0 +1,7 @@
+module SearchesHelper
+  def render_search_results(collection)
+   render :partial    => "searches/result",
+          :collection => collection,
+          :as         => :item,
+  end
+end
