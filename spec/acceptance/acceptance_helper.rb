a14c6f4e49f8cc457f62ca96d3362c0a4ab7e4d9
David Leal <david@mojotech.com>
2011-10-03 09:32:22 +0100

    [specs] Use webrick and specify output file for logs.

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -3,6 +3,15 @@ require 'spec_helper'
 Spork.each_run do
   Dir[Rails.root.join("spec/acceptance/support/**/*.rb")].each { |f| require f }
 
+  Capybara.server do |app, port|
+    require 'rack/handler/webrick'
+
+    logger = WEBrick::Log::new(Rails.root.join("log/acceptance_test.log").to_s)
+
+    Rack::Handler::WEBrick.
+      run(app, :Port => port, :AccessLog => [], :Logger => logger)
+  end
+
   RSpec.configure do |config|
     Capybara.javascript_driver = :webkit
 
