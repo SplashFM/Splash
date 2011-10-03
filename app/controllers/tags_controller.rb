e27145492b4c811efaef0f56a9e8d7b46cca0107
David Leal <david@mojotech.com>
2011-10-03 13:44:23 +0100

    Add limit to tag searches.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,13 @@
 class TagsController < ApplicationController
+  MAX_TAGS = 5
+
   def index
-    genres  = Genre.filter(params[:q])
-    artists = Artist.filter(params[:q])
-    hashes  = (artists + genres).map { |t|
+    # TODO: optimize
+    genres  = Genre.filter(params[:q]).limit(MAX_TAGS)
+    artists = Artist.filter(params[:q]).limit(MAX_TAGS)
+    hashes  = (artists + genres).sort_by(&:name).map { |t|
       t.as_json(:only => [:id, :name]).merge!(:type => t.class.name.underscore)
-    }
+    }.first(MAX_TAGS)
 
     render :json => hashes
   end
