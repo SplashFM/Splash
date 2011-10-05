74c53f0e559686483dab9f745425c5a6b45da1df
Chris Shoemaker <chris@mojotech.com>
2011-10-05 17:44:02 -0400

    Add Hipchat deploy notification hook.

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,19 @@
 node[:applications].each do |app, data|
   sudo "monit -g dj_#{app} restart all"
 end
+
+on_app_master do
+  #if @configuration[:environment] == 'production'
+    config = YAML.load_file File.join(release_path, 'config', 'hipchat.yml')
+    client = HipChat::Client.new config['token']
+    person = `whoami`
+
+    message  = "#{person} is deploying"
+    message += " revision #{@configuration[:revision]}"
+    message += " to #{@configuration[:environment]}"
+    message += " (with migrations)" if migrate?
+    message += "."
+
+    client[config['room']].send(person, message, true)
+  #end
+end
