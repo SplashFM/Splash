185945e60641a8591a6cc09f91d65b437d7825a7
Avand Amiri <avand@avandamiri.com>
2011-10-07 11:21:28 -0500

    Updating after_restart HipChat hook with shortened revision message

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,11 +3,10 @@ node[:applications].each do |app, data|
 end
 
 on_app_master do
-  message  = "Deploying revision #{@configuration[:revision]}"
+  message  = "Deploying revision #{@configuration[:revision][0...6]}"
   message += " to #{@configuration[:environment]}"
   message += " (with migrations)" if migrate?
   message += "."
 
-  # Send a message via rake task assuming a hipchat.yml in your config like above
   run "cd #{release_path} && rake hipchat:send MESSAGE='#{message}'"
 end
