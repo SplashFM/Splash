542fa9ea1711805cf02fa4143411b37c9b49037c
David Leal <david@mojotech.com>
2011-10-14 11:37:13 +0100

    Put all track upload specs in the same place.

--- spec/acceptance/upload_track_spec.rb
+++ spec/acceptance/upload_track_spec.rb
@@ -20,5 +20,51 @@ feature "Upload track", :js => true do
       should have(1).track
     end
   end
+
+  scenario "Splash uploaded", :driver => :selenium do
+    track = build(Track).
+      title("Steady As She Goes").
+      albums("An Airplane Carried Me to Bed").
+      with_performer!("Sky Sailing")
+
+    search_for "Nothing", :track do
+      upload file('sky_sailing_steady_as_she_goes.m4a'),
+             track,
+             'This is my comment!'
+    end
+
+    visit profile_path
+
+    with_splash Splash.first do
+      expand_track
+    end
+
+    should have_content("This is my comment!")
+  end
+
+  scenario "Splash uploaded that already exists", :driver => :selenium do
+    track = create(Track).
+      title("Steady As She Goes").
+      albums("An Airplane Carried Me to Bed").
+      with_performer!("Sky Sailing")
+
+    search_for "Nothing", :track do
+      upload file('sky_sailing_steady_as_she_goes.m4a'),
+             track,
+             'This is my comment!'
+    end
+
+    search_for "Steady As She Goes", :track do
+      should have(1).track
+    end
+
+    visit profile_path
+
+    with_splash Splash.first do
+      expand_track
+    end
+
+    should have_content("This is my comment!")
+  end
 end
 
