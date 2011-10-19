e5176c88b8c17c0a0bc0676a32c293cad1fe143d
David Leal <david@mojotech.com>
2011-10-19 11:48:58 +0100

    Wrap Redis counter handling.

--- lib/redis_record.rb
+++ lib/redis_record.rb
@@ -8,6 +8,32 @@ module RedisRecord
     def key(field)
       "#{Rails.env}/#{name.underscore}/#{field}"
     end
+
+    def redis_counter(name)
+      instance_eval <<-RUBYI
+        def increment_#{name.to_s.pluralize}(ids)
+          ids.each { |id| redis.hincrby key(#{name.to_s.inspect}), id.to_s, 1 }
+        end
+
+        def reset_#{name.to_s.pluralize}
+          redis.del key(#{name.to_s.inspect})
+        end
+
+        def #{name.to_s.pluralize}
+          redis.hgetall(key(#{name.to_s.inspect}))
+        end
+      RUBYI
+
+      class_eval <<-RUBY
+        def increment_#{name}
+          self.class.increment_#{name.to_s.pluralize}([id])
+        end
+
+        def #{name}
+          redis.hget(key(#{name.to_s.inspect}), id.to_s).to_i
+        end
+      RUBY
+    end
   end
 
   def self.included(base)
