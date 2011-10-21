3b936816b7ff06d8ec0518ad178e3988da16b100
David Leal <david@mojotech.com>
2011-10-21 15:39:07 +0100

    [redis] Split counter and sort functionality.

--- lib/redis_record.rb
+++ lib/redis_record.rb
@@ -17,28 +17,8 @@ module RedisRecord
       end
     end
 
-    def redis_counter(name)
+    def redis_sorted_field(name)
       instance_eval <<-RUBYI
-        def increment_#{name.to_s.pluralize}(ids)
-          ids.each { |id|
-            increment_#{name}(id)
-            increment_#{name}_sorted(id)
-          }
-        end
-
-        def increment_#{name}(id)
-          RedisRecord.redis.hincrby key(#{name.to_s.inspect}), id.to_s, 1
-        end
-
-        def increment_#{name}_sorted(id)
-          RedisRecord.redis.zincrby key("sorted_#{name}"), 1, id.to_s
-        end
-
-        def reset_#{name.to_s.pluralize}
-          RedisRecord.redis.del key(#{name.to_s.inspect})
-          RedisRecord.redis.del key("sorted_#{name}")
-        end
-
         def sorted_#{name.to_s.pluralize}(page, num_records)
           page  = page.to_i <= 1 ? 1 : page
           start = (page - 1) * num_records
@@ -52,6 +32,40 @@ module RedisRecord
           cache = Hash[*where(:id => ids).map { |t| [t.id, t] }.flatten]
 
           ids.map { |id| cache[id] }
+        end
+
+        def increment_#{name}_sorted(id)
+          RedisRecord.redis.zincrby key("sorted_#{name}"), 1, id.to_s
+        end
+
+        def reset_#{name}_sorted
+          RedisRecord.redis.del key("sorted_#{name}")
+        end
+      RUBYI
+    end
+
+    def redis_counter(name)
+      redis_sorted_field name
+
+      instance_eval <<-RUBYI
+        def increment_#{name.to_s.pluralize}(ids)
+          ids.each { |id|
+            increment_#{name}(id)
+            increment_#{name}_sorted(id)
+          }
+        end
+
+        def increment_#{name}(id)
+          RedisRecord.redis.hincrby key(#{name.to_s.inspect}), id.to_s, 1
+        end
+
+        def reset_#{name}_counter
+          RedisRecord.redis.del key(#{name.to_s.inspect})
+        end
+
+        def reset_#{name.to_s.pluralize}
+          reset_#{name}_counter
+          reset_#{name}_sorted
         end
 
         def #{name.to_s.pluralize}
