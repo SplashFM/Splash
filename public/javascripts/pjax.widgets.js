d8584d68400db1aa14762d1070b4dc30f5bd03b7
David Leal <david@mojotech.com>
2011-10-24 22:58:01 +0100

    Use pjax fork with HTML4 browser support.

--- public/javascripts/pjax.widgets.js
+++ public/javascripts/pjax.widgets.js
@@ -5,13 +5,12 @@ Widgets.Pjax = {
     $("#main a:not([data-remote]):not([data-skip-pjax]):not(.fancybox):not([data-widget = \"play\"]):not([href^=#])")
             .pjax('[data-pjax-container]', {timeout: 2500});
 
-    $('body').bind('pjax:end', function() {
+    $('body').bind('success.pjax', function() {
       Widgets.Feed.reload();
       Widgets.Search.reload();
     });
   }
 }
-
 
 $(document).ready(function() {
   Widgets.Pjax.init();
