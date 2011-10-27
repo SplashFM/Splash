b517e1b9810e7e1c9756488d3db8e1d2603f07ad
David Leal <david@mojotech.com>
2011-10-27 15:12:26 +0100

    Raise error if creating a Following notificatio fails.

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,8 +1,8 @@
 class Following < Notification
   def self.notify(relationship)
-    create(:notified => relationship.followed,
-           :notifier => relationship.follower,
-           :title    => I18n.t(:following, :scope => 'emails.subjects',
-                               :follower => relationship.follower.name))
+    create!(:notified => relationship.followed,
+            :notifier => relationship.follower,
+            :title    => I18n.t(:following, :scope => 'emails.subjects',
+                                :follower => relationship.follower.name))
   end
 end
