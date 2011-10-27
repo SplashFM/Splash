8a25b45795743ec14a4dd0b1ee669086e7b754c6
David Leal <david@mojotech.com>
2011-10-27 15:14:14 +0100

    Generalize relationship observer to handle many kinds of notifications.

--- app/models/notifications_observer.rb
+++ app/models/notifications_observer.rb
@@ -1,5 +1,13 @@
-class RelationshipObserver < ActiveRecord::Observer
-  def after_create(relationship)
+class NotificationsObserver < ActiveRecord::Observer
+  observe :relationship
+
+  def after_create(target)
+    send "notify_#{target.class.name.underscore}", target
+  end
+
+  private
+
+  def notify_relationship(relationship)
     Notification.create(:notified => relationship.followed,
                         :notifier => relationship.follower,
                         :title => I18n.t(:following, :scope => 'emails.subjects', :follower => relationship.follower.name))
