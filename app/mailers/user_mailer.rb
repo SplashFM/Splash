c79de82023525417cc2d8cecd7c5e4f484751a82
David Leal <david@mojotech.com>
2011-11-03 21:58:07 +0000

    Restore sending email notification on new following.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,10 @@ class UserMailer < ActionMailer::Base
   default :from => "notifications@splash.fm"
 
   def notification(notification)
-    mail(:to      => notification.notified.email,
-         :subject => t(notification.title, :notification => notification))
+    @notification = notification
+
+    mail :to            => notification.notified.email,
+         :subject       => t(notification.title),
+         :template_name => notification.class.name.underscore
   end
 end
