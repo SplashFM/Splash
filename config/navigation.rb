14bc311dc852c802054f13e08d2aa910de30eeb6
David Leal <david@mojotech.com>
2011-11-04 20:50:39 +0000

    Restore navigation.

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 # Configures your navigation
 SimpleNavigation::Configuration.run do |navigation|
+  navigation.selected_class = 'current-page'
   navigation.items do |primary|
+    primary.dom_class = 'right avan-demi'
+    primary.dom_id    = 'navigation'
+
     primary.item :home, t('simple_navigation.menus.home'), home_url
     if logged_in?
       primary.item :profile, t('simple_navigation.menus.profile'), profile_url()
