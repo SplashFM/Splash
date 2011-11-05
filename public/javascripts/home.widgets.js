cae60194e2884bdf143e4b57814aaad7a01188ca
David Leal <david@mojotech.com>
2011-11-05 21:49:04 +0000

    Make reusable search class from home track search view.

--- public/javascripts/home.widgets.js
+++ public/javascripts/home.widgets.js
@@ -1,63 +1,18 @@
 $(function() {
-  window.HomeTrackSearch = Backbone.View.extend({
-    delay: 500,
-
-    el: '[data-widget = "track-search"]',
-
-    events: {
-      'keyup :text': 'maybeSearch'
-    },
-
-    template: $('#tmpl-home-track').template(),
-
+  window.Home = Backbone.View.extend({
     initialize: function() {
-      this.tracks  = new TrackList
-      this.results = this.$('[data-widget = "results"]');
-      this.menu    = $('ul', this.results);
-
-      _.bindAll(this, 'search', 'renderTrack');
-
-      this.tracks.bind('reset', this.render, this)
-    },
-
-    isSearchable: function() {
-      return this.term().length > 0 && this.lastTerm !== this.term();
-    },
-
-    maybeSearch: function() {
-      if (this.timeout) clearTimeout(this.timeout);
-
-      this.timeout  = setTimeout(this.search, this.delay);
-    },
-
-    render: function() {
-      this.menu.empty();
-      this.tracks.each(this.renderTrack);
-      this.menu.find('li:last').addClass('last');
-
-      this.results.show();
-    },
-
-    renderTrack: function(t) {
-      this.menu.append($.tmpl(this.template, t.toJSON()));
-    },
-
-    search: function() {
-      if (this.isSearchable()) {
-        this.lastTerm = this.term();
-
-        this.tracks.fetch({data: {with_text: this.term()}});
-      }
-    },
-
-    term: function() {
-      return this.$(':text').val();
+      this.trackSearch = new Home.TrackSearch()
     }
   });
 
-  window.Home = Backbone.View.extend({
-    initialize: function() {
-      this.trackSearch = new HomeTrackSearch
+  Home.TrackSearch = Search.extend({
+    collection: new TrackList,
+    el: '[data-widget = "track-search"]',
+    menuContainer: 'ul',
+    template: '#tmpl-home-track',
+
+    open: function() {
+      this.menu.find('li:last').addClass('last');
     }
   });
 
---
+++ public/javascripts/search.widgets.js
@@ -0,0 +1,56 @@
+$(function() {
+  window.Search = Backbone.View.extend({
+    container: '[data-widget = "results"]',
+
+    delay: 500,
+
+    events: {
+      'keyup :text': 'maybeSearch'
+    },
+
+    initialize: function() {
+      this.container = this.$(this.container);
+      this.menu      = this.$(this.menuContainer || this.container);
+      this.template  = $(this.template).template()
+
+      _.bindAll(this, 'search', 'renderItem');
+
+      this.collection.bind('reset', this.render, this)
+    },
+
+    isSearchable: function() {
+      return this.term().length > 0 && this.lastTerm !== this.term();
+    },
+
+    maybeSearch: function() {
+      if (this.timeout) clearTimeout(this.timeout);
+
+      this.timeout  = setTimeout(this.search, this.delay);
+    },
+
+    render: function() {
+      this.menu.empty();
+      this.collection.each(this.renderItem);
+
+      if (this.open) this.open.call(this);
+
+      this.container.show();
+    },
+
+    renderItem: function(i) {
+      return $($.tmpl(this.template, i.toJSON())).appendTo(this.menu);
+    },
+
+    search: function() {
+      if (this.isSearchable()) {
+        this.lastTerm = this.term();
+
+        this.collection.fetch({data: {with_text: this.term()}});
+      }
+    },
+
+    term: function() {
+      return this.$(':text').val();
+    }
+  });
+});
