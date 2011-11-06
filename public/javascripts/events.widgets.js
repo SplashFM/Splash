b7c3f01f947312b1c55b4fb599253b4d86f29ade
David Leal <david@mojotech.com>
2011-11-06 17:04:03 +0000

    Allow toggling event filter visibility.

--- public/javascripts/events.widgets.js
+++ public/javascripts/events.widgets.js
@@ -19,6 +19,8 @@ $(function() {
       this.feed.fetch({data: {user: this.currentUserId,
                               follower: this.currentUserId,
                               update_on_splash: true}})
+
+      this.filter = new Events.Filter;
     },
 
     render: function() {
@@ -29,6 +31,31 @@ $(function() {
 
     renderEvent: function(e) {
       $($.tmpl(this.templates[e.get('type')], e.toJSON())).appendTo(this.el);
+    },
+  });
+
+  var HEIGHT_WHEN_OPEN = 62;
+
+  window.Events.Filter = Backbone.View.extend({
+    el: '[data-widget = "events-filter"]',
+    events : {
+      'click [data-widget = "toggle"]' : 'toggleFilter'
+    },
+
+    initialize: function() {
+      this.filter = this.$('[data-widget = "filter"]');
+
+      _.bindAll(this, 'toggleFilter');
+    },
+
+    toggleFilter: function() {
+      this.filter.toggle();
+
+      if (this.filter.is(':visible')) {
+        $(this.el).height(HEIGHT_WHEN_OPEN);
+      } else {
+        $(this.el).height('auto');
+      }
     }
   });
 });
