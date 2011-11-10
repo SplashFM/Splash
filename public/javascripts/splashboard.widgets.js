7839e335260e2f003bbbeda0f45cd97218b97ada
Chris Shoemaker <chris@mojotech.com>
2011-11-10 15:32:09 -0500

    Add a backbone view for top tracks on the splashboard.

---
+++ public/javascripts/splashboard.widgets.js
@@ -0,0 +1,50 @@
+$(function() {
+  window.Splashboard = Backbone.View.extend({
+    el: '[data-widget = "top-tracks"]',
+    template: $('#tmpl-home-track').template(),
+
+    initialize: function(opts) {
+      _.extend(this, opts);
+
+      _.bindAll(this, 'scroll', 'renderTrack');
+
+      this.pageFilters = {user: this.currentUserId,
+                          follower: this.currentUserId,
+                          update_on_splash: true}
+      this.userFilters = {};
+
+      this.feed = new TrackList;
+      this.feed.bind('reset', this.render, this);
+      this.feed.bind('add', this.renderTrack, this);
+
+      this.fetch();
+
+      this.page = 1;
+      //this.app.bind('endlessScroll', this.feed.scroll, this)
+
+    },
+    allFilters: function() {
+      return {top: true};
+    },
+
+    fetch: function(add) {
+      this.feed.fetch({add:  add,
+                       data: _.extend({page: this.page},
+                                      this.allFilters())});
+    },
+    scroll: function() {
+      this.page++;
+
+      this.fetch(true);
+    },
+    render: function() {
+      $(this.el).empty();
+      this.feed.each(this.renderTrack);
+      return this;
+    },
+    renderTrack: function(s) {
+      var json       = _.extend({}, s.toJSON());
+      $($.tmpl(this.template, json)).appendTo(this.el);
+    },
+  });
+});
