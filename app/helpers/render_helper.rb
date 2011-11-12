296167624851ca4581661460adc674e7e56943b6
Chris Shoemaker <chris@mojotech.com>
2011-11-12 11:40:46 -0500

    Remove some obsolete code related to event rendering.

--- app/helpers/render_helper.rb
+++ app/helpers/render_helper.rb
@@ -1,10 +1,4 @@
 module RenderHelper
-  def render_events(path_opts)
-    render :partial => "events/index",
-           :as      => :events,
-           :locals  => {:base_url => events_path(path_opts)}
-  end
-
   def render_upload_form(stage, track = UndiscoveredTrack.new, status = :ok)
     render :partial => "tracks/#{stage}",
            :status  => status,
@@ -12,20 +6,4 @@ module RenderHelper
 
   end
 
-  def render_event_list(events = [])
-    render :partial => "events/list",
-           :object  => events,
-           :as      => :events,
-           :locals => {:last_update_at => Event.timestamp}
-
-  end
-
-  private
-
-  def url_for_event_updates(events, id)
-    url_for(:action      => 'event_updates',
-            :controller  => controller_name,
-            :id          => id,
-            :last_update => Time.now.iso8601)
-  end
 end
