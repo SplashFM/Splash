a54b4a5cac9f62b64428f683d1b4a398beacb76e
Franco Brusatti <fbrusatti@gmail.com>
2011-11-16 12:26:48 -0300

    Add nickname to users table

--- db/schema.rb
+++ db/schema.rb
@@ -191,6 +191,7 @@ ActiveRecord::Schema.define(:version => 20111116162050) do
     t.string   "slug"
     t.date     "birthday"
     t.text     "ignore_suggested_users"
+    t.string   "nickname"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
