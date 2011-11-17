6ad0c52b863b7ba561c7f800a25f9d7fea723b90
Chris Shoemaker <chris@mojotech.com>
2011-11-16 22:01:56 -0500

    Enable splash button dimming for unsplashable tracks on the splashboard.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -6,6 +6,7 @@ class TracksController < ApplicationController
   has_scope :with_text
 
   def index
+    sth = current_user.try(:splashed_tracks_hash)
     if params[:user_id] && user = User.find(params[:user_id])
       results = user.top_tracks(current_page, TRACKS_PER_PAGE)
     elsif params[:top]
@@ -13,7 +14,8 @@ class TracksController < ApplicationController
     else
       results = apply_scopes(Track).page(current_page)
     end
-    respond_with results
+
+    respond_with results.as_json(:splashed_tracks => sth)
   end
 
   def top
