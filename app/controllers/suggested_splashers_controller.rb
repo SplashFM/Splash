2b59af991c87278ef8326e7c6484b302307cf123
Franco Brusatti <fbrusatti@gmail.com>
2011-11-18 01:13:14 -0300

    [Fix] Able to ignore recommended splashers again

--- app/controllers/suggested_splashers_controller.rb
+++ app/controllers/suggested_splashers_controller.rb
@@ -1,12 +1,19 @@
 class SuggestedSplashersController < ApplicationController
+  before_filter :load_user, :only => :destroy
+
   def index
     render :partial => "shared/suggested_splashers"
   end
 
  def destroy
-    user = User.find_by_slug(params[:id])
-    current_user.ignore_suggested(user)
+    current_user.ignore_suggested(@user)
 
     render :partial => "users/suggested_splasher", :collection => current_user.recommended_users, :as => :user
+  end
+
+private
+
+  def load_user
+    @user = User.find_by_slug(params[:id]) || User.find(params[:id])
   end
 end
