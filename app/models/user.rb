099561ea808e1cc31b30594f4db1fa6ef9f97ddb
Chris Shoemaker <chris@mojotech.com>
2011-11-17 23:52:13 -0500

    Link to profile pages from top splashers splashboard.

--- app/models/user.rb
+++ app/models/user.rb
@@ -168,7 +168,7 @@ class User < ActiveRecord::Base
   def as_json(opts = {})
     {:id            => id,
      :name          => name,
-     :url          => "#{slug}",
+     :url          => "/#{slug}",
      :avatar_search => avatar.url(:thumb),
      :ripple_count => ripple_count,
      :splash_count => splash_count,
