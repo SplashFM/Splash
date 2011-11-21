6bcc036a41f794ce0f466389f909cda729fb6960
Chris Shoemaker <chris@mojotech.com>
2011-11-21 10:56:40 -0500

    Run cron jobs on app_master instead of util for now.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,7 +1,9 @@
 @rails_env = node[:environment][:framework_env]
 whenever_command = "bundle exec whenever"
 
-if ['solo', 'util'].include?(node[:instance_role])
+#cron_instances = ['solo', 'util']
+cron_instances = ['solo', 'app_master']
+if cron_instances.include?(node[:instance_role])
   node[:applications].each do |app, data|
     run "cd #{release_path} && #{whenever_command} --update-crontab '#{app}' --set environment=#{@rails_env}"
   end
