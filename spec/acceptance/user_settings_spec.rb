f27f836fbea7a2fe00d3e50397a1022f1013710c
David Leal <david@mojotech.com>
2011-11-23 23:30:48 +0000

    [tests] Check if a user's password can be changed.

--- spec/acceptance/user_settings_spec.rb
+++ spec/acceptance/user_settings_spec.rb
@@ -16,5 +16,18 @@ feature "User settings", :js => true do
       page.should have_field(t(User, :nickname), :with => 'ior3k')
     }
   end
+
+  scenario "Successfully update the password" do
+    edit_settings {
+      fill_in t(User, :password), :with => 'cowabunga!'
+      fill_in 'user_password_confirmation', :with => 'cowabunga!'
+    }
+
+    log_out
+    go_to 'home', :no_pjax
+    fast_login user.email, 'cowabunga!'
+
+    page.should be_home
+  end
 end
 
