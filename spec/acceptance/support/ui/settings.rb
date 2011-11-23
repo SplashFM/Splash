d980cd495c26eff628bdf2c9b48ff688b380a990
David Leal <david@mojotech.com>
2011-11-23 22:46:42 +0000

    [tests] Add reusable actions for settings.

---
+++ spec/acceptance/support/ui/settings.rb
@@ -0,0 +1,22 @@
+module UI
+  module Settings
+    def self.included(base)
+      base.send(:include, Actions)
+    end
+
+    module Actions
+      def edit_settings(&actions)
+        with_settings {
+          actions.call
+          click_button t('devise.registrations.edit.save')
+        }
+      end
+
+      def with_settings(&do_stuff)
+        with_lifesaver { click_link t('lifesaver.settings') }
+
+        within w('settings'), &do_stuff
+      end
+    end
+  end
+end
