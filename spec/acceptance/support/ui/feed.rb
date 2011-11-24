d0a1c01d48891a552bb943a4724e0d316a623adb
David Leal <david@mojotech.com>
2011-11-24 16:51:59 +0000

    Add filter-related test helpers.

---
+++ spec/acceptance/support/ui/feed.rb
@@ -0,0 +1,48 @@
+require 'acceptance/support/ui/base'
+
+module UI
+  module Feed
+    def self.included(base)
+      base.send(:include, Actions)
+
+      base.before {
+        page.extend(Collections)
+        page.extend(Matchers)
+      }
+    end
+
+    module Actions
+      def enable(filter)
+        case filter
+        when :mentions
+          click_link t('events.filters.mentions')
+        else
+          raise "Unknown filter: #{filter}"
+        end
+
+      end
+
+      def with_feed(&do_stuff)
+        within w('feed'), &do_stuff
+      end
+    end
+
+    module Collections
+      def mentions
+        wait_until { all(w('mention')).presence }
+      end
+
+      def splashes
+        wait_until { all('splash').presence }
+      end
+    end
+
+    module Matchers
+      include Base::Helpers
+
+      def has_no_splashes?
+        has_no_css?(w('splash'))
+      end
+    end
+  end
+end
