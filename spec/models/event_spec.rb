b157b5bc43a89596633de8bb4730478189f17a55
David Leal <david@mojotech.com>
2011-11-24 15:22:03 +0000

    Add basic specs for Event.

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Event do
+  [
+   ['comments', :other, Comment],
+   ['relationships', :other, Relationship],
+   ['splashes', :splashes, Splash],
+  ].each { |(label, filter, klass)|
+    it "includes #{label}" do
+      create!(klass)
+
+      events = Event.scope_by(filter => 1)
+      events.should have(1).item
+      events.each { |e| e.should be_an_instance_of(klass) }
+    end
+
+    it "filters out #{label}" do
+      create!(klass)
+
+      Event.scope_by({}).should be_empty
+    end
+  }
+end
