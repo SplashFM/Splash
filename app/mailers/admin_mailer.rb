c5efc29572dc30e2e339b060208fa0e662f33432
David Leal <david@mojotech.com>
2011-11-29 20:45:50 +0000

    Make flag email address configurable.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ActionMailer::Base
   default :from => "Splash.FM <notifications@splash.fm>"
 
   def flag(song, user)
-    mail :to            => 'report@splash.fm',
+    mail :to            => AppConfig.email[:flag],
          :subject       => "Song #{song.title} flagged by #{user.nickname}"
   end
 end
