1d598caafc3126296d3f9eda5bef84cd87179aea
David Leal <david@mojotech.com>
2011-11-29 21:02:22 +0000

    [flagging] Pass values to email templates properly.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,8 @@ class AdminMailer < ActionMailer::Base
   default :from => "Splash.FM <notifications@splash.fm>"
 
   def flag(song, user)
+    @user, @song = user, song
+
     mail :to            => AppConfig.email[:flag],
          :subject       => "Song #{song.title} flagged by #{user.nickname}"
   end
