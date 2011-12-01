4389b3450630eb40bd3e9f47e46f63cd34c78126
David Leal <david@mojotech.com>
2011-12-01 16:35:12 +0000

    Remove useless scenario from feed tests.

--- spec/acceptance/shared/feed.rb
+++ spec/acceptance/shared/feed.rb
@@ -1,17 +1,6 @@
 shared_examples_for "feed" do
   include UI::Feed
 
-  scenario "Shows #{Event::PER_PAGE} items at a time" do
-    11.times { create(Splash).user!(user) }
-
-    go_to current_page
-
-    feed {
-      should have(10).splashes
-      should have_no_loading_spinner
-    }
-  end
-
   describe "Endless scrolling" do
     include UI::Feed
 
