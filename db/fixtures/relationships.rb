824a2ea1f68b8dfef24171f158257ee11729665a
David Leal <david@mojotech.com>
2011-12-01 22:01:56 +0000

    Make generic seed users follow the main seed user.

--- db/fixtures/relationships.rb
+++ db/fixtures/relationships.rb
@@ -9,3 +9,11 @@ Relationship.seed(:follower_id, :followed_id) { |r|
   r.follower_id = User.find_by_email('jack.close@mojotech.com').id
   r.followed_id = User.find_by_email('jack.johnson@mojotech.com').id
 }
+
+mojo_user = User.find_by_email('user@mojotech.com')
+User.where("name like ?", "Mojo User %").each do |u|
+  Relationship.seed(:follower_id, :followed_id) { |r|
+    r.follower_id = u.id
+    r.followed_id = mojo_user.id
+  }
+end
