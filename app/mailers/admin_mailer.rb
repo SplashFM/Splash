887a510f04af8980d67aefb47abc38de9487baf4
David Leal <david@mojotech.com>
2011-12-05 21:08:05 +0000

    [flag] Use string as key instead of symbol (thanks Franco).

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,7 +4,7 @@ class AdminMailer < ActionMailer::Base
   def flag(song, user)
     @user, @song = user, song
 
-    mail :to            => AppConfig.email[:flag],
+    mail :to            => AppConfig.email['flag'],
          :subject       => "Song #{song.title} flagged by #{user.nickname}"
   end
 end
