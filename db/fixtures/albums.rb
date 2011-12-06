c6010b42de3d0600f952cd9c47475095a807f97d
David Leal <david@mojotech.com>
2011-12-06 15:37:42 +0000

    Remove unused fixtures.

--- db/fixtures/albums.rb
+++
@@ -1,9 +0,0 @@
-if ! Rails.env.production? || ENV['FORCE_SEED'] == '1'
-  Album.seed :name do |g|
-    g.name = 'Close to the edge'
-  end
-
-  Album.seed :name do |g|
-    g.name = 'Nevermind'
-  end
-end
