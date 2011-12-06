e7214f53aaad6a67d41866774af0175f8fb95c9a
David Leal <david@mojotech.com>
2011-12-06 21:30:23 +0000

    Add shared test for user splash tree thumbnail display.

--- spec/acceptance/shared/feed.rb
+++ spec/acceptance/shared/feed.rb
@@ -1,6 +1,28 @@
 shared_examples_for "feed" do
   include UI::Feed
 
+  describe "Splashes" do
+    scenario "Load splash tree" do
+      friend1 = create!(User)
+      friend2 = create!(User)
+      track   = create!(Track)
+
+      user.following = [friend1, friend2]
+
+      create(Splash).user(friend1).track!(track)
+      splash = create(Splash).user(user).track!(track)
+      create(Splash).user(friend2).with_parent!(splash)
+
+      go_to current_page
+
+      with_splash(:first) {
+        expand_splash
+
+        should have_ordered_splasher_thumbnails(friend1, user, friend2)
+      }
+    end
+  end
+
   describe "Endless scrolling" do
     include UI::Feed
 
