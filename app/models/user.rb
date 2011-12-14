ab061fb1b50cd71e502f54746fc06bf517187974
Chris Shoemaker <chris@mojotech.com>
2011-12-13 19:50:21 -0500

    Remove dead code in User#to_slug.
    It is impossible for the split(".") to find anything other than the whole
    string because the gsub(/\W+/, "_") will already have replaced all "."
    characters.

--- app/models/user.rb
+++ app/models/user.rb
@@ -573,7 +573,7 @@ class User < ActiveRecord::Base
 
   def to_slug(string)
     if !string.blank?
-      string.strip.gsub(/@/, "_at_").gsub(/\W+/, '_').split(".").first.downcase
+      string.strip.gsub(/@/, "_at_").gsub(/\W+/, '_').downcase
     end
   end
 
