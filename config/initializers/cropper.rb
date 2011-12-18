4fca718be09218e65bd8a04474845a5308e6f9ee
David Leal <david@mojotech.com>
2011-12-18 11:35:22 +0000

    Rewrite cropper preprocessor.

--- config/initializers/cropper.rb
+++ config/initializers/cropper.rb
@@ -1,25 +1,24 @@
 module Paperclip
   class Cropper < Thumbnail
-    def transformation_command
-      crop_options = crop_command
-      current_geometry_small = @current_geometry.height < @target_geometry.height &&
-                                    @current_geometry.width < @target_geometry.width
-
-      if crop_options && current_geometry_small
-        crop_options
-
-      elsif crop_options && !current_geometry_small
-        crop_options + super.join(' ').sub(/ -crop \S+/, '').split(' ')
-
-      elsif !crop_options && current_geometry_small
-        ["-resize", "#{@current_geometry.width}x"]
-
-      else
-        super
-      end
+    def square?
+      @target_geometry.height.to_i == @target_geometry.width.to_i
     end
 
-    def crop_command
+    def transformation_command
+      super.each_slice(2).map { |(sw, val)|
+        if sw == '-crop'
+          [sw, user_geometry || transform_geometry(val)]
+        else
+          [sw, val]
+        end
+      }.compact
+    end
+
+    def transform_geometry(geom)
+      geom.gsub(/\d+"$/, '0"')
+    end
+
+    def user_geometry
       target = @attachment.instance
 
       if target.cropping?
@@ -28,12 +27,8 @@ module Paperclip
         crop_w = target.crop_w
         crop_h = square? ? target.crop_w : target.crop_h
 
-        ["-crop", "#{crop_w}x#{crop_h}+#{crop_x}+#{crop_y}"]
+        "#{crop_w}x#{crop_h}+#{crop_x}+#{crop_y}"
       end
-    end
-
-    def square?
-      @target_geometry.height.to_i == @target_geometry.width.to_i
     end
   end
 end
