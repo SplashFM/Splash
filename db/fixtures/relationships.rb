e3aa30ec0b8ca9c55a69785d2113d7980acdeb68
David Leal <david@mojotech.com>
2011-12-19 12:25:46 +0000

    Have seeds produce suggested users.

--- db/fixtures/relationships.rb
+++ db/fixtures/relationships.rb
@@ -11,6 +11,7 @@ Relationship.seed(:follower_id, :followed_id) { |r|
 }
 
 mojo_user = User.find_by_email('user@mojotech.com')
+close     = User.find_by_email('jack.close@mojotech.com')
 
 1.upto(30) do |i|
   u = User.find_by_name("Mojo User #{i}")
@@ -19,7 +20,15 @@ mojo_user = User.find_by_email('user@mojotech.com')
     r.follower_id = u.id
     r.followed_id = mojo_user.id
   }
+
+  Relationship.seed(:follower_id, :followed_id) { |r|
+    r.follower_id = close.id
+    r.followed_id = u.id
+  }
 end
+
+# weird
+mojo_user.suggest_users
 
 31.upto(60) do |i|
   u = User.find_by_name("Mojo User #{i}")
