9ca7f8231c44136ee6c47e0cfaeca335ebbd03b8
David Leal <david@mojotech.com>
2011-12-20 09:53:57 +0000

    Add the ability to get available access codes from a file.

--- app/models/access_request.rb
+++ app/models/access_request.rb
@@ -1,7 +1,13 @@
 class AccessRequest < ActiveRecord::Base
+  ACCESS_CODES_PATH = File.join(Rails.root, %w(config access_codes.yml))
+
   validates :email, :presence => true, :uniqueness => true
 
   before_create :reset_granted
+
+  def self.codes
+    @codes ||= YAML.load_file(ACCESS_CODES_PATH)
+  end
 
   private
 
