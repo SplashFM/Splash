bf577b847351a54cc1bc5435a521685cbdd227ca
David Leal <david@mojotech.com>
2011-12-30 14:11:55 +0000

    Index users on name.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111229205551) do
+ActiveRecord::Schema.define(:version => 20111230140238) do
 
   create_table "access_requests", :force => true do |t|
     t.string   "email"
@@ -213,6 +213,7 @@ ActiveRecord::Schema.define(:version => 20111229205551) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["name"], :name => "index_users_on_name"
   add_index "users", ["nickname"], :name => "index_users_on_nickname", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
