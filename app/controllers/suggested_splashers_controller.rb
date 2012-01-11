88497e2fd36f704f8eb0c8576811a06f36e73566
David Leal <david@mojotech.com>
2012-01-11 12:50:34 +0000

    Remove unused code.

--- app/controllers/suggested_splashers_controller.rb
+++ app/controllers/suggested_splashers_controller.rb
@@ -1,10 +1,6 @@
 class SuggestedSplashersController < ApplicationController
   respond_to :json
 
-  def index
-    respond_with current_user.recommended_users(current_page)
-  end
-
   def destroy
     current_user.ignore_suggested(params[:id])
 
