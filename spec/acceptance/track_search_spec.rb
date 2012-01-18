5ce3c2164860a9f1b79324257b9cb4894113485c
David Leal <david@mojotech.com>
2012-01-18 13:58:05 +0000

    [specs] Remove obsolete scenario.

--- spec/acceptance/track_search_spec.rb
+++ spec/acceptance/track_search_spec.rb
@@ -41,24 +41,4 @@ feature "Track search box", :adapter => :postgresql, :js => true do
 
     track_results { should have_no_load_more_results_link }
   end
-
-  scenario "Paginated results" do
-    pending
-
-    per_page     = Track.default_per_page
-    pages        = 3
-    total_tracks = per_page * pages
-
-    1.upto(total_tracks) { |i| create(Track).title!("Track #{i}") }
-
-    search_for "Track", :track do
-      wait_until(10) { page.has_more_results? }
-
-      1.upto(pages) { |p| see_more_results }
-
-      1.upto(total_tracks) { |i| should have_track("Track #{i}") }
-
-      pending { should_not have_more_results }
-    end
-  end
 end
