7940ad68b3a3dba7a793da49ef811b1d3d380e3f
David Leal <david@mojotech.com>
2012-01-27 11:37:15 +0000

    [friends] Add splash relationship to listing.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,8 +3,21 @@ class FriendsController < ApplicationController
     token   = current_user.social_connection('facebook').token
     friends = FbGraph::User.me(token).friends
 
-    @users  = User.
+    users = User.
       with_social_connection('facebook', friends.map(&:identifier)).
       by_score
+
+    rels = current_user.relationships.with_following(users.map(&:id))
+    relh = Hash[*rels.map { |r| [r.followed_id, r] }.flatten]
+
+    @users = users.map { |u|
+      j = u.as_json
+
+      j[:relationship] = (relh[u.id] ||
+                          {:follower_id => current_user.id,
+                           :followed_id => u.id}).as_json
+
+      j
+    }
   end
 end
--- public/javascripts/friends.widgets.js
+++ public/javascripts/friends.widgets.js
@@ -24,11 +24,22 @@ $(function() {
 
   window.FriendView = Backbone.View.extend({
     template: $('#tmpl-user').template(),
+    templateRelationship: $('#tmpl-relationship-list').template(),
 
     render: function() {
       $(this.el).html($.tmpl(this.template, this.model.toJSON()));
 
+      this.renderAction();
+
       return this;
-    }
+    },
+
+    renderAction: function() {
+      return new RelationshipView({
+        el: this.$('.right .follow-links'),
+        model:    new Relationship(this.model.get('relationship')),
+        template: this.templateRelationship,
+      }).render();
+    },
   });
 });
