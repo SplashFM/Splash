b0d3aa2e36f8998664aed5625f145d0d101a412a
David Leal <david@mojotech.com>
2012-01-31 14:26:17 +0000

    Configure redis cache.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -8,6 +8,7 @@ Scaphandrier::Application.configure do
   # Full error reports are enabled and caching is turned on
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
+  config.cache_store = :redis_store, {:key_prefix => Rails.env}
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
