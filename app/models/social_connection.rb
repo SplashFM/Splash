1b372f3455d6c3487269bda833efdae8201bfbf2
David Leal <david@mojotech.com>
2012-02-01 15:41:45 +0000

    [social connections] Modularize provider operations.

--- app/models/social_connection.rb
+++ app/models/social_connection.rb
@@ -1,5 +1,10 @@
 class SocialConnection < ActiveRecord::Base
+  extend Forwardable
+
   belongs_to :user
+
+  def_delegators :remote, :friends
+
   validates :uid, :uniqueness => true
 
   def self.with_provider(provider)
@@ -7,4 +12,36 @@ class SocialConnection < ActiveRecord::Base
   end
 
   alias_method :refresh, :update_attributes!
+
+  def remote
+    @remote ||= self.class.const_get(provider.classify).new(self)
+  end
+
+  class Facebook
+    def initialize(connection)
+      @me = FbGraph::User.new(connection.uid, :access_token => connection.token)
+    end
+
+    def friends
+      @me.friends
+    end
+
+    module FriendAttributes
+      def uid
+        identifier
+      end
+
+      FbGraph::User.send(:include, self)
+    end
+  end
+
+  class Twitter
+    def initialize(connection)
+      @connection = connection
+    end
+
+    def friends
+      []
+    end
+  end
 end
