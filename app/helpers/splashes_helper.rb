4c4c22b318a6c3baaaf903cb5a2adcf9905c1c34
David Leal <david@mojotech.com>
2012-02-02 12:59:50 +0000

    Remove unused code.

--- app/helpers/splashes_helper.rb
+++ app/helpers/splashes_helper.rb
@@ -1,21 +1,4 @@
 module SplashesHelper
-  def self.dom_id(splash)
-    "splash_#{splash.id}"
-  end
-
-  def expand_splash(splash)
-    link_to(t(".expand"),
-            splash_path(splash),
-            :remote        => true,
-            :'data-type'   => 'html',
-            :'data-widget' => 'splash-info-toggle')
-  end
-
-  def attribute_splash(splash)
-    t('.by', :user => link_to_user(splash.user)).html_safe unless
-      splash.owned_by?(current_user)
-  end
-
   def splash_comment_form(splash)
     form = form_for [splash, splash.comments.build],
                 :remote => true,
