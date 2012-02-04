ac7a380d29320b9aadce632fde33ba58db3ac4d7
David Leal <david@mojotech.com>
2012-02-04 21:58:32 +0000

    Add back proper action to mention and comment for splasher.

--- app/models/comment_for_splasher.rb
+++ app/models/comment_for_splasher.rb
@@ -2,4 +2,8 @@ class CommentForSplasher < CommentNotification
   def template
     'comment_notification'
   end
+
+  def action
+    I18n.t('notifications.comment_for_splasher')
+  end
 end
--- app/models/mention.rb
+++ app/models/mention.rb
@@ -2,4 +2,8 @@ class Mention < CommentNotification
   def template
     'mention'
   end
+
+  def action
+    I18n.t('notifications.mention')
+  end
 end
