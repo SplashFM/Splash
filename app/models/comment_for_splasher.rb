c40d524f840ca2e8e24b70d18c2b3bdc643c4b00
David Leal <david@mojotech.com>
2012-02-05 11:36:38 +0000

    Remove unnecessary methods.

--- app/models/comment_for_splasher.rb
+++ app/models/comment_for_splasher.rb
@@ -1,9 +1,2 @@
 class CommentForSplasher < CommentNotification
-  def template
-    'comment_notification'
-  end
-
-  def action
-    I18n.t('notifications.comment_for_splasher')
-  end
 end
--- app/models/mention.rb
+++ app/models/mention.rb
@@ -2,8 +2,4 @@ class Mention < CommentNotification
   def template
     'mention'
   end
-
-  def action
-    I18n.t('notifications.mention')
-  end
 end
