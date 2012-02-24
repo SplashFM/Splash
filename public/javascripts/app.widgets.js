56efd8fbec78858bab7c368a53b9799df786df1b
David Leal <david@mojotech.com>
2012-02-24 09:55:49 +0000

    Remove unused TrackSearch widget.

--- public/javascripts/app.widgets.js
+++ public/javascripts/app.widgets.js
@@ -311,27 +311,6 @@ $(function() {
     },
   });
 
-  window.BaseApp.TrackSearch = Search.extend({
-    collection: new TrackList,
-    el: '[data-widget = "global-search"]',
-    menuContainer: '[data-widget = "tracks"] ul',
-    template: '#tmpl-global-search-track',
-
-    cycle: function(item, even, odd) {
-      if (this.currentCycle != odd) {
-        this.currentCycle = odd;
-      } else {
-        this.currentCycle = even;
-      }
-
-      if (this.currentCycle) item.addClass(this.currentCycle);
-    },
-
-    renderItem: function(i) {
-      this.cycle(Search.prototype.renderItem.call(this, i), 'even', 'odd');
-    }
-  });
-
   window.PlayerView = Backbone.View.extend({
     events: {'splash:quick': 'disableSplashButton'},
 
