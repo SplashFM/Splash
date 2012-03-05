5dfd1972e41ce75b36731db120c4e210eaea1581
David Leal <david@mojotech.com>
2012-03-05 13:15:00 +0000

    Fix "dump format error", er, error.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-  :key => "_#{Rails.application.name.downcase}_session"
+  :key => "_#{Rails.application.name.downcase}_3_2_session"
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
