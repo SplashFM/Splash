6bfe0b5670cd20a806a6e6cde5e57a3bbd3dc032
David Leal <david@mojotech.com>
2012-03-14 12:42:48 +0000

    Remove unused code.

--- public/javascripts/profile.widgets.js
+++ public/javascripts/profile.widgets.js
@@ -1,40 +1,4 @@
 $(function() {
-  window.ProfileController = Backbone.View.extend({
-    initialize: function() {
-      this.trackSearch = new TrackSearch({perPage: this.options.tracksPerPage});
-      this.eventFeed = new Events({
-        app:           window.App,
-        currentUserID: this.options.userID,
-        filters:       {user: this.options.userID},
-        updateFilters: {user: this.options.userID, splashes: 1},
-      });
-      this.scroll = new EndlessScroll({
-        data: this.eventFeed,
-        noMoreResults: $('<p/>').
-          text(I18n.t('events.all_loaded')).
-          addClass('loaded'),
-        spinnerContainer: $('.loading-spinner-container'),
-      });
-      this.relationship = new RelationshipView({
-        className: 'follow-container',
-        model:     new Relationship(this.options.relationship),
-        template:  $('#tmpl-profile-relationship').template(),
-      });
-
-      this.allResults = new TrackSearch.AllResults();
-    },
-
-    render: function() {
-      if (! this.options.isOwner) {
-        this.$('.user-vcard > div').append(this.relationship.render().el);
-      }
-
-      this.allResults.render();
-
-      return this;
-    }
-  }).extend(WithAllResults);
-
   window.RelationshipView = Backbone.View.extend({
     events: {'click a': 'toggle'},
 
