e725b8521b8b8573763691efea00e19fda23303b
David Leal <david@mojotech.com>
2012-03-20 16:38:49 +0000

    Remove no longer needed tasks from deploy process.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -10,6 +10,4 @@ if cron_instances.include?(node[:instance_role])
 end
 
 run "cd #{release_path} && bundle exec rails runner -e #{environment} 'Babilu.generate'"
-run "cd #{release_path} && bundle exec rake RAILS_ENV=#{environment} sass:compile"
-run "cd #{release_path} && bundle exec compass compile -e production"
 run "cd #{release_path} && RAILS_ENV=#{environment} bundle exec jammit  --base-url http://ec2-107-20-221-189.compute-1.amazonaws.com"
