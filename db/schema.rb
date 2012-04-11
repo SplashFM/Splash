b1078fdcdc53b57c1c5806d77a3082ed40e85dba
David Leal <david@mojotech.com>
2012-04-11 11:40:08 +0100

    Add missing devise column to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120329135206) do
+ActiveRecord::Schema.define(:version => 20120411114535) do
 
   create_table "access_requests", :force => true do |t|
     t.string   "email"
@@ -220,6 +220,7 @@ ActiveRecord::Schema.define(:version => 20120329135206) do
     t.text     "email_preferences"
     t.text     "settings"
     t.integer  "top_splasher_weight"
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
