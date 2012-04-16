3695a6917b36b09db48524f63634ccd4836cae61
David Leal <david@mojotech.com>
2012-04-16 09:37:16 +0100

    Restore unauthenticated homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,7 @@ class HomeController < ApplicationController
   skip_before_filter :require_user
 
   def index
-    if logged_in?
-      render text: '', layout: 'home'
-    else
-      redirect_to new_user_session_path
-    end
+    render text: '', layout: 'home'
   end
 
   def r
