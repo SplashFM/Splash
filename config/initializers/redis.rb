1572758792e9b55ceb60b00483b0c5fe4943bffe
Chris Shoemaker <chris@mojotech.com>
2012-04-17 16:05:19 -0400

    [redis] Set Redis.current to the configured redis connection.
    This solves a problem where Redis::SortedSet used Redis.current

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-Redis::Objects.redis = Redis.new(:host => AppConfig.redis['host'],
-                                 :port => AppConfig.redis['port'])
+redis = Redis.new(:host => AppConfig.redis['host'],
+                  :port => AppConfig.redis['port'])
+Redis::Objects.redis = redis
+Redis.current = redis
