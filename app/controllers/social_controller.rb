f828d250cd38743c276c32f6413b95e1a20371b4
Umer Aslam <umer.aslam@sprd.co>
2012-05-18 15:42:16 +0500

    facebook api change. fb_uid removed as params

--- app/controllers/social_controller.rb
+++ app/controllers/social_controller.rb
@@ -6,23 +6,22 @@ class SocialController < ApplicationController
   TWITTER_API_URL = "https://api.twitter.com/1/users/show.json?user_id="
   
   def signin_fb
-		if params[:token].present? and params[:uid].present?
-			user = User.with_social_connection("facebook", params[:uid])
-			if user 
-				user.social_connection("facebook").
-					refresh params.slice(:token)
-					
-				respond_with user
+		if params[:token].present?
+			ft = social_token_fb
+			if ft[:error].present?
+				respond_with ft
 			else
-				ft = social_token_fb
-				if ft[:error].present?
-					respond_with ft
-				else
-					user = User.create_with_social_connection(ft)
+				user = User.with_social_connection("facebook", ft[:uid])
+				if user 
+					user.social_connection("facebook").
+						refresh params.slice(:token)
+					
 					respond_with user
-				end
-			  
-			end				
+				else
+				  user = User.create_with_social_connection(ft)
+					respond_with user
+				end				
+			end
 		end
 	end
 	
@@ -44,13 +43,11 @@ class SocialController < ApplicationController
 					respond_with user
 				end
 			end				
-		else
-			respond_with {:error => "params missing"}			
 		end
 	end
 
 
-private:
+private
 	
 	def social_token_fb
 		@data = HTTParty.get(FACEBOOK_API_URL + params[:token]).parsed_response
@@ -63,12 +60,12 @@ private:
 				end
 			else
 			ft = {:provider=>"facebook",
-				 :uid=>params[:uid],
+				 :uid=>@data["id"],
 				 :token=>params[:token],
 				 :access_code=>nil,
 				 :email=>@data["email"],
 				 :name=>@data["name"],
-				 :nickname=>nil } 
+				 :nickname=>@data["username"] } 
 			end
 		end
 	end
