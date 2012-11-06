224e1be27c416b6eba943c7f868cbe18f707ddff
Umer Aslam <umer.aslam@sprd.co>
2012-11-06 19:40:05 +0500

    Use Distinct in sql query to remove duplicate tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,22 +1,26 @@
 class TagsController < ApplicationController
-  MAX_TAGS = 5
+  MAX_TAGS = 6
   SUGGEST_TAGS = 5
   def index
     # TODO: optimize
     hashes  = 
       if params[:q].present?
-        ActsAsTaggableOn::Tag.named_like(params[:q]).limit(SUGGEST_TAGS)
-                                     .sort_by(&:name).map { |t|
-                                       {:value => t.name}
-                                     }
+        ActsAsTaggableOn::Tag.named_like(params[:q]).limit(SUGGEST_TAGS).sort_by(&:name)
       else
-        tags = Comment.tag_counts_on(:tags).limit(MAX_TAGS).order('count desc')
-                                     .map { |t|
-                                       {:value => t.name}
-                                     }
+        #Comment.tag_counts_on(:tags).limit(MAX_TAGS).order('count desc') # query returning duplicate tags aswell
+        #ToDo: Clean query -> use distinct/pluck through ActiveRecord  
+        q = " SELECT DISTINCT tags.name, taggings.tags_count AS count
+              FROM tags JOIN (SELECT taggings.tag_id, COUNT(taggings.tag_id) AS tags_count
+              FROM taggings INNER JOIN comments ON comments.id = taggings.taggable_id
+              WHERE (taggings.taggable_type = 'Comment' AND taggings.context = 'tags')
+                AND (taggings.taggable_id IN(SELECT comments.id FROM comments ))
+              GROUP BY taggings.tag_id HAVING COUNT(taggings.tag_id) > 0) AS taggings ON taggings.tag_id = tags.id
+              ORDER BY count desc LIMIT #{MAX_TAGS}"
+        
+          ActsAsTaggableOn::Tag.find_by_sql(q)     
       end  
 
-    render :json => hashes
+    render :json => hashes.map { |t| {:value => t.name} }
   end
   
 end
